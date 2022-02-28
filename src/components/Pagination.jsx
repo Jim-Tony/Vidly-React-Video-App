532207e73995620c2ff88c01c392f553b14538f0
Jim-Tony <jimtonystarc@gmail.com>
2022-02-28 14:34:31 +0530

    component created

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import _ from 'lodash';
+const Pagination = (props) => {
+    const {itemCount,pageSize}  = props;
+    const pagesCount = Math.ceil(itemCount/pageSize);
+    if(pagesCount===1) return null;
+    const pages = _.range(1,pagesCount+1);
+    return (
+        <nav className="Page navigation example">
+            <ul className="pagination">
+                {pages.map(page=>
+                    <li key={page} className='page-item'><a style={{cursor:'pointer'}} className="page-link">{page}</a></li>    
+                )}
+            </ul>
+        </nav>         
+    );
+}
+ 
+export default Pagination;
