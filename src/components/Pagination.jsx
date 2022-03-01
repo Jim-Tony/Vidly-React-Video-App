7e45a000763112d1094dc96a424f038d44af8391
Jim-Tony <jimtonystarc@gmail.com>
2022-03-01 13:39:56 +0530

    styled the cursor

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 const Pagination = (props) => {
-    const {itemCount,pageSize}  = props;
+    const {itemCount,pageSize,onPageChange,currentPage}  = props;
     const pagesCount = Math.ceil(itemCount/pageSize);
     if(pagesCount===1) return null;
     const pages = _.range(1,pagesCount+1);
@@ -9,7 +9,7 @@ const Pagination = (props) => {
         <nav className="Page navigation example">
             <ul className="pagination">
                 {pages.map(page=>
-                    <li key={page} className='page-item'><a style={{cursor:'pointer'}} className="page-link">{page}</a></li>    
+                    <li key={page} className={currentPage===page?'page-item active':'page-item'}><a style={{cursor:'pointer'}} onClick={()=>onPageChange(page)} className="page-link">{page}</a></li>    
                 )}
             </ul>
         </nav>         
