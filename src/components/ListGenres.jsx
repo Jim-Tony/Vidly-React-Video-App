9f092603f5e67cc801861a043c3100de09fae779
Jim-Tony <jimtonystarc@gmail.com>
2022-03-02 11:40:16 +0530

    started ListGenres Comp

---
+++ src/components/ListGenres.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { getGenres } from '../services/fakeGenreService';
+const ListMovies = (props) => {
+    return (  
+        <ul class="list-group">
+            <li class="list-group-item">All Genres</li>
+        </ul>
+    );
+}
+ 
+export default ListMovies;
