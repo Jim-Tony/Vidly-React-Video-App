60cd1971639f04e0f64bf704779e36089ef2f504
Jim-Tony <jimtonystarc@gmail.com>
2022-03-04 14:17:09 +0530

    done sorting

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ import {getMovies} from './services/fakeMovieService';
 import { getGenres } from './services/fakeGenreService';
 import {paginate} from './utils/paginate';
 import PropTypes from 'prop-types';
+import _ from 'lodash';
 class App extends Component {
   state = {
     currentPage:1,
     pageSize:4,
     movies:[],
     genres:[],
+    currentGenre:{_id:'',name:'All Genres'},
+    sortColumn:{path:'title',order:'asc'}
   };
   componentDidMount(){
-    const genres = [{id:'_id',name:'All Genres'},...getGenres()];
+    const genres = [{_id:'',name:'All Genres'},...getGenres()];
     this.setState({movies:getMovies(),genres});
   }
   handleLike = movie=>{
@@ -34,10 +37,24 @@ class App extends Component {
   handleGenreSelect = genre=>{
     this.setState({currentGenre:genre,currentPage:1});
   }
+  handleSort = path=>{
+    const sortColumn = {...this.state.sortColumn};
+    if(sortColumn.path===path){
+      sortColumn.order = (sortColumn.order==='asc')?'desc':'asc';
+    }
+    else{
+      sortColumn.order = 'asc';
+      sortColumn.path = path;
+    }
+    this.setState({sortColumn});
+  }
   render() { 
-    const {currentPage,pageSize,currentGenre,movies:moviesAll} = this.state;
+    const {currentPage,pageSize,currentGenre,movies:moviesAll,sortColumn} = this.state;
+    //Filtering
     const filtered = (currentGenre && currentGenre._id) ? moviesAll.filter(m=>m.genre._id===currentGenre._id) : moviesAll;
-    const movies = paginate(filtered,currentPage,pageSize);
+    //Sorting 
+    const sortedMovies = _.orderBy(filtered,[sortColumn.path],[sortColumn.order]);
+    const movies = paginate(sortedMovies,currentPage,pageSize);
     return (
       <main className='container'>
         <div className='row'>
@@ -53,6 +70,7 @@ class App extends Component {
               movies={movies}
               onDelete = {this.handleDelete}  
               onClick = {this.handleLike}
+              onSort = {this.handleSort}
             />
             <Pagination 
               itemCount = {filtered.length}
--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -3,7 +3,7 @@ import Like from './Like';
 const Movies = (props) => {
     const {length:count} = props.movies;
     if(count===0) return (<p className='lead mt-5'>There are no movies in the database</p>);
-    const {movies,onClick,onDelete} = props;
+    const {movies,onClick,onDelete,onSort} = props;
     return (
             <div>
                 <p className="mt-5 lead">Showing {count} movies in the database</p>
