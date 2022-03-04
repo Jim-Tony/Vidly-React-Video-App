3e9f3831d3022b7c2c54cd2c77383b825065dce5
Jim-Tony <jimtonystarc@gmail.com>
2022-03-04 11:40:32 +0530

    raised sort event

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,38 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Like from './Like';
-class Movies extends Component{
-    render(){
-        const {length:count} = this.props.movies;
-        if(count===0) return (<p className='lead mt-5'>There are no movies in the database</p>);
-        return(
+const Movies = (props) => {
+    const {length:count} = props.movies;
+    if(count===0) return (<p className='lead mt-5'>There are no movies in the database</p>);
+    const {movies,onClick,onDelete} = props;
+    return (
             <div>
                 <p className="mt-5 lead">Showing {count} movies in the database</p>
                 <table className="table">
                     <thead>
                         <tr>
-                            <th>Title</th>
-                            <th>Genre</th>
-                            <th>Stock</th>
-                            <th>Rate</th>
+                            <th onClick={()=>onSort('title')}>Title</th>
+                            <th onClick={()=>onSort('genre.name')}>Genre</th>
+                            <th onClick={()=>onSort('numberInStock')}>Stock</th>
+                            <th onClick={()=>onSort('dailyRentalRate')}>Rate</th>
                             <th />
                             <th />
                         </tr>
                     </thead>
                     <tbody>
-                        {this.props.movies.map(movie=>(
+                        {movies.map(movie=>(
                             <tr key={movie._id}>
                                 <td>{movie.title}</td>
                                 <td>{movie.genre.name}</td>
                                 <td>{movie.numberInStock}</td>
                                 <td>{movie.dailyRentalRate}</td>
-                                <td><Like liked={movie.liked} onClick={()=>this.props.onClick(movie)}/></td>
-                                <td><button className='btn btn-danger' onClick={()=>this.props.onDelete(movie)}>Delete</button></td>
+                                <td><Like liked={movie.liked} onClick={()=>onClick(movie)}/></td>
+                                <td><button className='btn btn-danger' onClick={()=>onDelete(movie)}>Delete</button></td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
             </div>
         );
-    }
 }
+ 
 export default Movies;
