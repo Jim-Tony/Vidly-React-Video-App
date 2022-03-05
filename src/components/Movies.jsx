5523671ede634adbd1f2ba0c7bf8307fe926f06a
Jim-Tony <jimtonystarc@gmail.com>
2022-03-05 13:49:12 +0530

    created tableheader comp

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,36 +1,28 @@
 import React,{Component} from 'react';
 import Like from './Like';
+import TableHeader from './TableHeader';
 class Movies extends Component {
-    raiseSort = path=>{
-        const sortColumn = {...this.props.sortColumn};
-        if(sortColumn.path===path){
-          sortColumn.order = (sortColumn.order==='asc')?'desc':'asc';
-        }
-        else{
-          sortColumn.order = 'asc';
-          sortColumn.path = path;
-        }
-        this.props.onSort(sortColumn);
-    }
+    columns = [
+        {path:'title',label:'Title'},
+        {path:'genre.name',label:'Genre'},
+        {path:'numberInStock',label:'Stock'},
+        {path:'dailyRentalRate',label:'Rate'},
+        // {key:'emptyOne'},
+        // {key:'emptyTwo'},
+    ]
     render() { 
         const {length:count} = this.props.movies;
         if(count===0) return (<p className='lead mt-5'>There are no movies in the database</p>);
-        const {movies,onClick,onDelete} = this.props;
-
+        const {movies,onClick,onDelete,sortColumn,onSort} = this.props;
         return (
             <div>
                 <p className="mt-5 lead">Showing {count} movies in the database</p>
                 <table className="table">
-                    <thead>
-                        <tr>
-                            <th onClick={()=>this.raiseSort('title')}>Title</th>
-                            <th onClick={()=>this.raiseSort('genre.name')}>Genre</th>
-                            <th onClick={()=>this.raiseSort('numberInStock')}>Stock</th>
-                            <th onClick={()=>this.raiseSort('dailyRentalRate')}>Rate</th>
-                            <th />
-                            <th />
-                        </tr>
-                    </thead>
+                    <TableHeader 
+                        columns={this.columns}
+                        sortColumn = {sortColumn}
+                        onSort={onSort}
+                    />
                     <tbody>
                         {movies.map(movie=>(
                             <tr key={movie._id}>
