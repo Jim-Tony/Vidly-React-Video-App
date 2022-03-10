1ecad724aeba95d5ff4cdac8640dc172eb86ef1b
Jim-Tony <jimtonystarc@gmail.com>
2022-03-10 13:16:30 +0530

    added movie form comp

---
+++ src/components/movieForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const MovieForm = () => {
+    return ( 
+        <div>
+            <h1 className='m-5'>Movie Form</h1>
+            <div><button className="btn btn-primary">Save</button></div>
+        </div>
+    );
+}
+ 
+export default MovieForm;
