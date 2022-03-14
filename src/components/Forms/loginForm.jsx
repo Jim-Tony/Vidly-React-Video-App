96384e5dfdcb967599e6c3e40dd8cefd41beb6a9
Jim-Tony <jimtonystarc@gmail.com>
2022-03-14 17:07:56 +0530

    handled multiple inputs

--- src/components/Forms/loginForm.jsx
+++ src/components/Forms/loginForm.jsx
@@ -3,26 +3,27 @@ class LoginForm extends Component {
     state={
         account:{username:'',password:''},
     };
-    handleChange = e=>{
+    handleChange = ({currentTarget:input})=>{
         const account = {...this.state.account};
-        account.username = e.currentTarget.value
+        account[input.name] = input.value
         this.setState({account});
     }
-    handleSubmit = e=>{
+    handleSubmit = e=>{ 
         e.preventDefault();
     };
     render() { 
+        const {account} = this.state;
         return (
             <div className="w-25 m-5">
                 <h1>Login</h1>
                 <form onSubmit={this.handleSubmit}>
                     <div className='form-group mb-3'>
                         <label className='form-label' htmlFor="username">User Name</label>
-                        <input onChange={this.handleChange} value={this.state.account.username} autoFocus autoComplete='off' className='form-control' id='username' type="text" />
+                        <input name='username' onChange={this.handleChange} value={account.username} autoFocus autoComplete='off' className='form-control' id='username' type="text" />
                     </div>
                     <div className='form-group mb-3'>
                         <label className='form-label' htmlFor="password">Password</label>
-                        <input className='form-control' id='password' type="password" />
+                        <input name='password' onChange={this.handleChange} value={account.password} className='form-control' id='password' type="password" />
                     </div>
                     <button className="btn btn-primary">Submit</button>
                 </form>    
