5b62300c6810fcb7fac0c5e56b8a1d30f24c3941
Jim-Tony <jimtonystarc@gmail.com>
2022-03-14 20:47:17 +0530

    object destructing

--- src/components/Forms/formGroup.jsx
+++ src/components/Forms/formGroup.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 const FormGroup = (props) => {
+    const {name,label,onChange,value,type} = props;
     return ( 
         <div className='form-group mb-3'>
-            <label className='form-label' htmlFor={props.name}>{props.label}</label>
-            <input name={props.name} onChange={props.onChange} value={props.value} autoFocus={props.name==='username'?true:false} autoComplete='off' className='form-control' id={props.name} type={props.type} />
+            <label className='form-label' htmlFor={name}>{label}</label>
+            <input name={name} onChange={onChange} value={value} autoFocus={name==='username'?true:false} autoComplete='off' className='form-control' id={name} type={type} />
         </div>
     );
 }
