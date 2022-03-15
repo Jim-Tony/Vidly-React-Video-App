a673bbfd33371627291284fe0c63390352a26ee3
Jim-Tony <jimtonystarc@gmail.com>
2022-03-15 23:16:55 +0530

    basic validation done

--- src/components/Forms/formGroup.jsx
+++ src/components/Forms/formGroup.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 const FormGroup = (props) => {
-    const {name,label,onChange,value,type} = props;
+    const {name,label,onChange,value,type,error} = props;
     return ( 
         <div className='form-group mb-3'>
             <label className='form-label' htmlFor={name}>{label}</label>
-            <input name={name} onChange={onChange} value={value} autoFocus={name==='username'?true:false} autoComplete='off' className='form-control' id={name} type={type} />
+            <input  name={name} onChange={onChange} value={value} autoFocus={name==='username'?true:false} autoComplete='off' className='form-control' id={name} type={type} />
+            {error && <div className="alert alert-danger">{error}</div>}
         </div>
     );
 }
--- src/components/Forms/loginForm.jsx
+++ src/components/Forms/loginForm.jsx
@@ -3,17 +3,41 @@ import FormGroup from './formGroup';
 class LoginForm extends Component { 
     state={
         account:{username:'',password:''},
+        errors:{},
     };
+    validateProperty = ({name,value})=>{
+        if(name==='username'){
+            if(value.trim()==='') return 'Username is required'
+        }
+        if(name==='password'){
+            if(value.trim()==='') return 'Password is required'
+        }
+    }
     handleChange = ({currentTarget:input})=>{
+        const errors = {...this.state.errors};
+        const errorMsg = this.validateProperty(input);
+        if(errorMsg) errors[input.name] = errorMsg;
         const account = {...this.state.account};
         account[input.name] = input.value
-        this.setState({account});
+        this.setState({account,errors});
+    }
+    validate = ()=>{
+        const {account} = this.state;
+        const errors = {};
+        if(account.username.trim()==='') errors.username = 'Username is required';
+        if(account.password.trim()==='') errors.password = 'Password is required'; 
+        return Object.keys(errors).length === 0 ? null:errors;
     }
     handleSubmit = e=>{ 
         e.preventDefault();
+        const errors = this.validate();
+        this.setState({errors:errors||{}});
+        // console.log(errors);
+        if(errors) return;
+        
     };
     render() { 
-        const {account} = this.state;
+        const {account,errors} = this.state;
         return (
             <div className="center">
                 <h1 className='text-center'>Login</h1>
@@ -24,6 +48,7 @@ class LoginForm extends Component {
                         value={account.username}
                         type="text"
                         onChange={this.handleChange}
+                        error={errors.username}
                     />
                     <FormGroup
                         name="password"
@@ -31,6 +56,7 @@ class LoginForm extends Component {
                         value={account.password}
                         type="password"
                         onChange={this.handleChange}
+                        error={errors.password}
                     />
                     <button className="btn btn-primary btn-center">Login</button>
                 </form>    
